293914d7cb9a2488d1d1bb9f0af0819c1b433f45
jamilyVitoria15 <vjamily484@gmail.com>
2026-06-08 20:36:17 -0300

    feat(T01): criar enum TipoVeiculo

---
+++ src/ParkSys/enums/TipoVeiculo.java
@@ -0,0 +1,31 @@
+package ParkSys.enums;
+
+public enum TipoVeiculo {
+
+    MOTO("Motocicleta", 5.0, 1),
+    CARRO("Automóvel", 10.0, 1),
+    SUV("SUV", 18.0, 2),
+    CAMINHAO("Caminhão", 30.0, 3);
+
+    private final String descricao;
+    private final double tarifaHora;
+    private final int vagasOcupadas;
+
+    TipoVeiculo(String descricao, double tarifaHora, int vagasOcupadas) {
+        this.descricao = descricao;
+        this.tarifaHora = tarifaHora;
+        this.vagasOcupadas = vagasOcupadas;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getTarifaHora() {
+        return tarifaHora;
+    }
+
+    public int getVagasOcupadas() {
+        return vagasOcupadas;
+    }
+}
