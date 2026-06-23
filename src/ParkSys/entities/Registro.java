9f85b94815ba710251639c95151187f9046a251c
Jamily Vitoria <seu-email-do-github@example.com>
2026-06-22 21:12:00 -0300

    feat(C05): implementa interface Comparable em Registro para ordenacao cronologica

--- src/ParkSys/entities/Registro.java
+++ src/ParkSys/entities/Registro.java
@@ -3,7 +3,7 @@ package ParkSys.entities;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class Registro implements Serializable {
+public class Registro implements Serializable, Comparable<Registro> { // Adicionado Comparable
 
     private static final long serialVersionUID = 1L;
 
@@ -12,9 +12,7 @@ public class Registro implements Serializable {
     private LocalDateTime dataEntrada;
     private LocalDateTime dataSaida;
     private double valorPago;
-
-    //(M04 e M07)
-    private transient String threadOrigem;
+    private transient String threadOrigem; // Para uso futuro com Threads
 
     public Registro() {
     }
@@ -23,6 +21,15 @@ public class Registro implements Serializable {
         this.veiculo = veiculo;
         this.vaga = vaga;
         this.dataEntrada = LocalDateTime.now();
+    }
+
+    // Implementacao do Requisito C05 para ordenacao cronologica automatica
+    @Override
+    public int compareTo(Registro outro) {
+        if (this.dataEntrada == null || outro.dataEntrada == null) {
+            return 0;
+        }
+        return this.dataEntrada.compareTo(outro.dataEntrada);
     }
 
     public Veiculo getVeiculo() {
@@ -77,7 +84,7 @@ public class Registro implements Serializable {
     public String toString() {
         return "Registro{" +
                 "veiculo=" + veiculo +
-                ", vaga=" + vaga +
+                ", vaga=" + (vaga != null ? vaga.getId() : "null") +
                 ", dataEntrada=" + dataEntrada +
                 ", dataSaida=" + dataSaida +
                 ", valorPago=" + valorPago +
