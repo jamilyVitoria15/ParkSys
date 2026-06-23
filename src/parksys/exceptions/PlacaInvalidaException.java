e033782656aa2eef63c4d227b255ef8876286007
Jamily Vitoria <seu-email-do-github@example.com>
2026-06-22 21:43:10 -0300

    feat(exceptions): adiciona PlacaInvalidaException para fechar o pacote de excecoes

---
+++ src/parksys/exceptions/PlacaInvalidaException.java
@@ -0,0 +1,9 @@
+package parksys.exceptions;
+
+public class PlacaInvalidaException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public PlacaInvalidaException(String mensagem) {
+        super(mensagem);
+    }
+}
